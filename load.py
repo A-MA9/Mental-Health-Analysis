766b923c75b1a1ecd6d2af31a073890dce0c238f
Aman Bharti <138147278+A-MA9@users.noreply.github.com>
2025-03-03 11:10:04 +0530

    Create load.py

---
+++ load.py
@@ -0,0 +1,20 @@
+import boto3
+import pickle
+
+bucket_name = "bucket_name"
+pickle_file_key = "file_path"  
+
+s3 = boto3.client("s3")
+
+local_pickle_path = "/tmp/stress_level_models.pkl"
+s3.download_file(bucket_name, pickle_file_key, local_pickle_path)
+
+with open(local_pickle_path, "rb") as f:
+    model_data = pickle.load(f)
+
+model = model_data["random_forest"]
+scaler = model_data["scaler"]
+encoder = model_data["encoder"]
+selected_features = model_data["selected_features"]
+
+print("Model loaded successfully from S3!")
